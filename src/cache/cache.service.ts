20704143551e6a93fe566de087101a2ca6e4ab64
thething0 <5640217+thething0@users.noreply.github.com>
2025-12-08 21:40:43 +0300

    Removed unused cache method

--- src/cache/cache.service.ts
+++ src/cache/cache.service.ts
@@ -4,14 +4,6 @@ export class CacheService {
   private readonly cache = new Map<string, unknown>();
   constructor(private readonly dbService: DatabaseService) {}
 
-  public warmUpMediaCache(): void {
-    const allMedia = this.dbService.getAllMediaCache();
-    for (const medium of allMedia) {
-      this.cache.set(medium.path, medium.file_id);
-    }
-    console.log(`[Cache] Warmed up with ${allMedia.length} media items.`);
-  }
-
   public getFileId(path: string): unknown {
     return this.cache.get(path);
   }
