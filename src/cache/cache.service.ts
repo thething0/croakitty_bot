2853ece8873317f2ef49a75ecd2b319c6034c7c2
thething0 <5640217+thething0@users.noreply.github.com>
2026-04-11 18:17:27 +0300

    Added cache preload

--- src/cache/cache.service.ts
+++ src/cache/cache.service.ts
@@ -5,7 +5,9 @@ import { DatabaseService } from '../database/database.service';
 @Injectable()
 export class CacheService {
   private readonly cache = new Map<string, unknown>();
-  constructor(private readonly dbService: DatabaseService) {}
+  constructor(private readonly dbService: DatabaseService) {
+    this.loadFromDB()
+  }
 
   public getFileId(path: string): unknown {
     return this.cache.get(path);
@@ -14,5 +16,12 @@ export class CacheService {
   public setFileId(path: string, fileId: string): void {
     this.cache.set(path, fileId);
     this.dbService.setFileId(path, fileId); // Сразу пишем и в БД
+  }
+
+  private loadFromDB(): void {
+    const records = this.dbService.getAllMediaCache();
+    for (const { path, file_id } of records) {
+      this.cache.set(path, file_id);
+    }
   }
 }
